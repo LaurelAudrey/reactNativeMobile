750cc8ace7ab7de379ee6f4c8aa82bfc6e7f361a
Laurel Elm <laurel.elm@gmail.com>
2022-12-04 19:29:53 -0700

    Push for Troubleshoot: Components Part 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,4 +1,4 @@
-import { FlatList } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 
 const DirectoryScreen = (props) => {
