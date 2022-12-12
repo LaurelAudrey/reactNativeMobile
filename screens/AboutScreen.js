c0e018aca2f8f54405d0bcd71a755baaa04fd368
Laurel Elm <laurel.elm@gmail.com>
2022-12-12 07:59:34 -0700

    Update AboutUs.js

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,39 +1,14 @@
 import { Text, ScrollView } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
-const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS)
-    
-    return (
-        <ScrollView>
-            <Mission />
-            <Card>
-                <Card.Title>Community Partners</Card.Title>
-                <Card.Divider />
-                { partners.map(partner => {
-                    return (
-                        <ListItem key={partner.id}>
-                            <Avatar source={partner.image} rounded/>
-                            <ListItem.Content>
-                                <ListItem.Title>{partner.name}</ListItem.Title>
-                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    )
-                })}
-            </Card>
-        </ScrollView>
-    );
-};
-
-const Mission = () => {
+function Mission() {
     return (
         <Card>
             <Card.Title>Our Mission</Card.Title>
             <Card.Divider />
-            <Text style={{margin:10}}>
+            <Text style={{ margin:10 }}>
                 We present a curated database of the best campsites in the vast woods 
                 and backcountry of the World Wide Web Wilderness. We increase access 
                 to adventure for the public while promoting safe and respectful use 
@@ -43,7 +18,37 @@ const Mission = () => {
                 they have visited with each other.
             </Text>
         </Card>
-    )
+    );
 }
+
+const AboutScreen = () => {
+    const partners = useSelector((state) => state.partners);
+
+    return (
+        <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.partnersArray.map((partner) => (
+                    <ListItem key={partner.id}>
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + partner.image }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{partner.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {partner.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
+            </Card>
+        </ScrollView>
+    );
+};
+
+
 
 export default AboutScreen;
